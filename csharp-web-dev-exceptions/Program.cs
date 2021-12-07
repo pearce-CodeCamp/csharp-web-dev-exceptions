8a3ad8d78b27c4256a3134413cb26f0358ff3d7e
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-12-07 16:14:29 -0600

    Completed and tested method exceptions

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,65 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+
+        // returns x divided y
+        // throws error when attempting to divide by 0
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            // if we try to divide by zero, throw exception
+            // if y is equal 0, throw exception
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("Attempting to divide by zero");
+            }
+            return x / y;
         }
 
+        // if the fileName ends in '.cs', return 1
+        // if the fileName doesn't end in '.cs', return 0
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            // if fileName is empty or null, throw exception
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException();
+            }
+            else if (fileName.EndsWith(".cs"))
+            {
+                // fileName ends in .cs
+                return 1;
+            }
+            // fileName doesn't end in .cs and isn't empty
+            return 0;
         }
-        */
+
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+
+            /*try
+            {
+                Console.WriteLine(Divide(1, 0));
+            } catch (ArgumentOutOfRangeException error)
+            {
+                Console.WriteLine("Attempted to divide by zero");
+            } finally
+            {
+                Console.WriteLine("This is the finally block executing");
+            }
+
+            try
+            {
+                Console.WriteLine(Divide(20, 5));
+            } catch (ArgumentOutOfRangeException error)
+            {
+                Console.WriteLine("Attempted to divide by zero");
+            } finally
+            {
+                Console.WriteLine("This is the finally block executing");
+            }*/
+
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -29,8 +73,20 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+            students.Add("Pearce", "index.html");
 
-
+            foreach(KeyValuePair<string, string> kvp in students)
+            {
+                try
+                {
+                    int score = CheckFileExtension(kvp.Value);
+                    Console.WriteLine($"{kvp.Key}'s score: {score}");
+                } catch (ArgumentNullException error)
+                {
+                    Console.WriteLine($"{kvp.Key} didn't provide a file.");
+                    Console.WriteLine(error.Message);
+                }
+            }
         }
     }
 }
